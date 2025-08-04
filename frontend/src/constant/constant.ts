40d69ce5e2fd7c9d864429aa59df74a804245dfc
Sushanta Bhowmick <sushanta@cetpainfotech.com>
2025-08-05 01:25:52 +0530

    Fix: Move deploy.yml to workflows directory

--- frontend/src/constant/constant.ts
+++ frontend/src/constant/constant.ts
@@ -1,5 +1,9 @@
-export const BASE_URL = "http://localhost:4000/api";
-export const SOCKET_BASE_URL = "http://localhost:4000";
+// export const BASE_URL = "http://localhost:4000/api";
+// export const SOCKET_BASE_URL = "http://localhost:4000";
+
+
+export const BASE_URL = "http://15.207.112.124/api";
+export const SOCKET_BASE_URL = "http://15.207.112.124";
 
 
 // export const BASE_URL = "https://communicate-t0ux.onrender.com/api";
